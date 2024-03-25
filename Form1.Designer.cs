e0943f83d5b690c3cffd7db75c593b6c00a25963
ChenChihWeiwei <162962360+ChenChihWeiwei@users.noreply.github.com>
2024-03-25 14:59:21 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAllClear = new System.Windows.Forms.Button();
+            this.txtCM = new System.Windows.Forms.TextBox();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.txtKM = new System.Windows.Forms.TextBox();
+            this.txtYard = new System.Windows.Forms.TextBox();
+            this.txtFt = new System.Windows.Forms.TextBox();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAllClear
+            // 
+            this.btnAllClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAllClear.Location = new System.Drawing.Point(69, 45);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(133, 55);
+            this.btnAllClear.TabIndex = 0;
+            this.btnAllClear.Text = "清除";
+            this.btnAllClear.UseVisualStyleBackColor = true;
+            this.btnAllClear.Click += new System.EventHandler(this.btnAllClear_Click);
+            // 
+            // txtCM
+            // 
+            this.txtCM.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCM.Location = new System.Drawing.Point(69, 177);
+            this.txtCM.Name = "txtCM";
+            this.txtCM.Size = new System.Drawing.Size(125, 38);
+            this.txtCM.TabIndex = 1;
+            this.txtCM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtCM_KeyUp);
+            // 
+            // txtM
+            // 
+            this.txtM.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtM.Location = new System.Drawing.Point(299, 177);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(125, 38);
+            this.txtM.TabIndex = 2;
+            this.txtM.TextChanged += new System.EventHandler(this.txtM_TextChanged);
+            this.txtM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtM_KeyUp);
+            // 
+            // txtKM
+            // 
+            this.txtKM.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKM.Location = new System.Drawing.Point(506, 177);
+            this.txtKM.Name = "txtKM";
+            this.txtKM.Size = new System.Drawing.Size(125, 38);
+            this.txtKM.TabIndex = 3;
+            this.txtKM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtKM_KeyUp);
+            // 
+            // txtYard
+            // 
+            this.txtYard.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYard.Location = new System.Drawing.Point(506, 288);
+            this.txtYard.Name = "txtYard";
+            this.txtYard.Size = new System.Drawing.Size(125, 38);
+            this.txtYard.TabIndex = 6;
+            this.txtYard.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtYard_KeyUp);
+            // 
+            // txtFt
+            // 
+            this.txtFt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFt.Location = new System.Drawing.Point(299, 288);
+            this.txtFt.Name = "txtFt";
+            this.txtFt.Size = new System.Drawing.Size(125, 38);
+            this.txtFt.TabIndex = 5;
+            this.txtFt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFt_KeyUp);
+            // 
+            // txtIn
+            // 
+            this.txtIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIn.Location = new System.Drawing.Point(69, 288);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(125, 38);
+            this.txtIn.TabIndex = 4;
+            this.txtIn.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtIn_KeyUp);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInfo.Location = new System.Drawing.Point(69, 393);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(562, 38);
+            this.txtInfo.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(69, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 24);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "公分";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(506, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(105, 24);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "公里";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(299, 137);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(105, 24);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "公尺";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(69, 250);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(105, 24);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "英吋";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(299, 250);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(105, 24);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "英呎";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(506, 250);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(105, 24);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.Text = "碼";
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(69, 352);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(105, 24);
+            this.textBox7.TabIndex = 14;
+            this.textBox7.Text = "說明文字";
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtYard);
+            this.Controls.Add(this.txtFt);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.txtKM);
+            this.Controls.Add(this.txtM);
+            this.Controls.Add(this.txtCM);
+            this.Controls.Add(this.btnAllClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAllClear;
+        private System.Windows.Forms.TextBox txtCM;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.TextBox txtKM;
+        private System.Windows.Forms.TextBox txtYard;
+        private System.Windows.Forms.TextBox txtFt;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 全域變數
+        string strInput; // 字串型態的strInput變數
+        double douOutput; // double浮點數型態的douOutput變數
+
+        private void txtCM_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtCM.Text; // 將txtCM文字框的值放入strInput變數
+
+            // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                txtM.Text = string.Format("{0:0.##########}", douOutput / 100);
+                txtKM.Text = string.Format("{0:0.##########}", douOutput / 100000);
+                txtIn.Text = string.Format("{0:0.##########}", douOutput / 2.54);
+                txtFt.Text = string.Format("{0:0.##########}", douOutput / 30.48);
+                txtYard.Text = string.Format("{0:0.##########}", douOutput / 91.44);
+            }
+            else
+            {
+                // 如果無法轉型，則是在說明文字中顯示錯誤訊息，並且將txtCM文字框清除
+                txtInfo.Text = "請輸入數字";
+                txtCM.Text = "";
+            }
+        }
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtM_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtM_KeyUp(object sender, KeyEventArgs e)
+        {
+            strInput = txtM.Text; // 將txtM文字框的值放入strInput變數
+
+            // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                txtCM.Text = string.Format("{0:0.##########}", douM / 0.01);
+                txtKM.Text = string.Format("{0:0.##########}", douM / 1000);
+                txtIn.Text = string.Format("{0:0.##########}", douM / 0.0254);
+                txtFt.Text = string.Format("{0:0.##########}", douM / 0.3048);
+                txtYard.Text = string.Format("{0:0.##########}", douM / 0.9144);
+            }
+            else
+            {
+                // 如果無法轉型，則是在說明文字中顯示錯誤訊息，並且將txtM文字框清除
+                txtInfo.Text = "請輸入數字";
+                txtM.Text = "";
+            }
+        }
+
+        private void txtKM_KeyUp(object sender, KeyEventArgs e)
+            {
+                strInput = txtKM.Text; 
+
+                // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
+                if (double.TryParse(strInput, out douOutput) == true)
+                {
+                    txtCM.Text = string.Format("{0:0.##########}", douKM / 0.00001);
+                    txtM.Text = string.Format("{0:0.##########}", douKM / 0.001);
+                    txtIn.Text = string.Format("{0:0.##########}", douKM / 0.0000254);
+                    txtFt.Text = string.Format("{0:0.##########}", douKM / 0.0003048);
+                    txtYard.Text = string.Format("{0:0.##########}", douKM / 0.0009144);
+                }
+                else
+                {
+                    // 如果無法轉型，則是在說明文字中顯示錯誤訊息，並且將txtM文字框清除
+                    txtInfo.Text = "請輸入數字";
+                    txtKM.Text = "";
+
+
+                }
+            }
+        private void txtIn_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douIn;
+
+            douIn = Convert.ToDouble(txtIn.Text);
+
+            txtCM.Text = string.Format("{0:0.##########}", douIn *2.54);
+            txtM.Text = string.Format("{0:0.##########}", douIn *0.0254);
+            txtKM.Text = string.Format("{0:0.##########}", douIn /39370.1);
+            txtFt.Text = string.Format("{0:0.##########}", douIn / 12);
+            txtYard.Text = string.Format("{0:0.##########}", douIn / 36);
+        }
+
+        private void txtFt_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douFt;
+
+            douFt = Convert.ToDouble(txtFt.Text);
+
+            txtCM.Text = string.Format("{0:0.##########}", douFt * 30.48);
+            txtM.Text = string.Format("{0:0.##########}", douFt * 0.3048);
+            txtKM.Text = string.Format("{0:0.##########}", douFt /3280.84);
+            txtIn.Text = string.Format("{0:0.##########}", douFt *12);
+            txtYard.Text = string.Format("{0:0.##########}", douFt / 3);
+        }
+
+        private void txtYard_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douYard;
+
+            douYard = Convert.ToDouble(txtYard.Text);
+
+            txtCM.Text = string.Format("{0:0.##########}", douYard * 91.44);
+            txtM.Text = string.Format("{0:0.##########}", douYard * 0.9144);
+            txtKM.Text = string.Format("{0:0.##########}", douYard * 0.0009144);
+            txtIn.Text = string.Format("{0:0.##########}", douYard * 36);
+            txtFt.Text = string.Format("{0:0.##########}", douYard * 3);
+        }
+
+        private void btnAllClear_Click(object sender, EventArgs e)
+        {
+            txtCM.Text = "";
+            txtM.Text = "";
+            txtKM.Text = "";
+            txtIn.Text = "";
+            txtFt.Text = "";
+            txtYard.Text = "";
+        }
+    }
+}
