752529ef834e5c0ff24cd9be7a6885bb716fe8a7
ChenChihWeiwei <162962360+ChenChihWeiwei@users.noreply.github.com>
2024-04-01 15:51:15 +0800

    20240401
    將程式碼寫成函式，但是啟動不了

--- Form1.cs
+++ Form1.cs
@@ -26,6 +26,7 @@ namespace WindowsFormsApp1
         string strInput; // 字串型態的strInput變數
         double douOutput; // double浮點數型態的douOutput變數
 
+        
         private void txtCM_KeyUp(object sender, EventArgs e)
         {
             strInput = txtCM.Text; // 將txtCM文字框的值放入strInput變數
@@ -33,11 +34,14 @@ namespace WindowsFormsApp1
             // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
             if (double.TryParse(strInput, out douOutput) == true)
             {
-                txtM.Text = string.Format("{0:0.##########}", douOutput / 100);
-                txtKM.Text = string.Format("{0:0.##########}", douOutput / 100000);
-                txtIn.Text = string.Format("{0:0.##########}", douOutput / 2.54);
-                txtFt.Text = string.Format("{0:0.##########}", douOutput / 30.48);
-                txtYard.Text = string.Format("{0:0.##########}", douOutput / 91.44);
+                txtInfo.Text = "";
+                caculateAnswer(0, douOutput);
+
+                //txtM.Text = string.Format("{0:0.##########}", douOutput / 100);
+                //txtKM.Text = string.Format("{0:0.##########}", douOutput / 100000);
+                //txtIn.Text = string.Format("{0:0.##########}", douOutput / 2.54);
+                //txtFt.Text = string.Format("{0:0.##########}", douOutput / 30.48);
+                //txtYard.Text = string.Format("{0:0.##########}", douOutput / 91.44);
             }
             else
             {
@@ -46,19 +50,8 @@ namespace WindowsFormsApp1
                 txtCM.Text = "";
             }
         }
-        private void textBox7_TextChanged(object sender, EventArgs e)
-        {
 
-        }
-
-        private void txtM_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        // 全域變數
-        string strInput; // 字串型態的strInput變數
-        double douOutput; // double浮點數型態的douOutput變數
+        
 
         private void txtM_KeyUp(object sender, KeyEventArgs e)
         {
@@ -67,11 +60,9 @@ namespace WindowsFormsApp1
             // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
             if (double.TryParse(strInput, out douOutput) == true)
             {
-                txtCM.Text = string.Format("{0:0.##########}", douM / 0.01);
-                txtKM.Text = string.Format("{0:0.##########}", douM / 1000);
-                txtIn.Text = string.Format("{0:0.##########}", douM / 0.0254);
-                txtFt.Text = string.Format("{0:0.##########}", douM / 0.3048);
-                txtYard.Text = string.Format("{0:0.##########}", douM / 0.9144);
+                txtInfo.Text = "";
+                caculateAnswer(1, douOutput * 100); // 事先將公尺轉換成公分
+                
             }
             else
             {
@@ -81,10 +72,6 @@ namespace WindowsFormsApp1
             }
         }
 
-        // 全域變數
-        string strInput; // 字串型態的strInput變數
-        double douOutput; // double浮點數型態的douOutput變數
-
         private void txtKM_KeyUp(object sender, KeyEventArgs e)
             {
                 strInput = txtKM.Text; 
@@ -92,11 +79,8 @@ namespace WindowsFormsApp1
                 // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
                 if (double.TryParse(strInput, out douOutput) == true)
                 {
-                    txtCM.Text = string.Format("{0:0.##########}", douKM / 0.00001);
-                    txtM.Text = string.Format("{0:0.##########}", douKM / 0.001);
-                    txtIn.Text = string.Format("{0:0.##########}", douKM / 0.0000254);
-                    txtFt.Text = string.Format("{0:0.##########}", douKM / 0.0003048);
-                    txtYard.Text = string.Format("{0:0.##########}", douKM / 0.0009144);
+                    txtInfo.Text = "";
+                    caculateAnswer(2, douOutput * 100000);
                 }
                 else
                 {
@@ -107,11 +91,6 @@ namespace WindowsFormsApp1
 
                 }
             }
-
-        // 全域變數
-        string strInput; // 字串型態的strInput變數
-        double douOutput; // double浮點數型態的douOutput變數
-
         private void txtIn_KeyUp(object sender, KeyEventArgs e)
         {
             strInput = txtIn.Text;
@@ -119,11 +98,9 @@ namespace WindowsFormsApp1
             // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
             if (double.TryParse(strInput, out douOutput) == true)
             {
-                txtCM.Text = string.Format("{0:0.##########}", douIn * 2.54);
-                txtM.Text = string.Format("{0:0.##########}", douIn * 0.0254);
-                txtKM.Text = string.Format("{0:0.##########}", douIn / 39370.1);
-                txtFt.Text = string.Format("{0:0.##########}", douIn / 12);
-                txtYard.Text = string.Format("{0:0.##########}", douIn / 36);
+                txtInfo.Text = "";
+                caculateAnswer(3, douOutput * 2.54);
+             
             }
             else
             {
@@ -133,10 +110,6 @@ namespace WindowsFormsApp1
             }
         }
 
-        // 全域變數
-        string strInput; // 字串型態的strInput變數
-        double douOutput; // double浮點數型態的douOutput變數
-
         private void txtFt_KeyUp(object sender, KeyEventArgs e)
         {
             strInput = txtFt.Text;
@@ -144,11 +117,10 @@ namespace WindowsFormsApp1
             // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
             if (double.TryParse(strInput, out douOutput) == true)
             {
-                txtCM.Text = string.Format("{0:0.##########}", douFt * 30.48);
-                txtM.Text = string.Format("{0:0.##########}", douFt * 0.3048);
-                txtKM.Text = string.Format("{0:0.##########}", douFt / 3280.84);
-                txtIn.Text = string.Format("{0:0.##########}", douFt * 12);
-                txtYard.Text = string.Format("{0:0.##########}", douFt / 3);
+                txtInfo.Text = "";
+                caculateAnswer(4, douOutput * 30.48);
+
+                
             }
             else
             {
@@ -159,10 +131,6 @@ namespace WindowsFormsApp1
 
         }
 
-        // 全域變數
-        string strInput; // 字串型態的strInput變數
-        double douOutput; // double浮點數型態的douOutput變數
-
         private void txtYard_KeyUp(object sender, KeyEventArgs e)
         {
             strInput = txtYard.Text;
@@ -170,11 +138,8 @@ namespace WindowsFormsApp1
             // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
             if (double.TryParse(strInput, out douOutput) == true)
             {
-                txtCM.Text = string.Format("{0:0.##########}", douYard * 91.44);
-                txtM.Text = string.Format("{0:0.##########}", douYard * 0.9144);
-                txtKM.Text = string.Format("{0:0.##########}", douYard * 0.0009144);
-                txtIn.Text = string.Format("{0:0.##########}", douYard * 36);
-                txtFt.Text = string.Format("{0:0.##########}", douYard * 3);
+                txtInfo.Text = "";
+                caculateAnswer(5, douOutput * 91.44);
             }
             else
             {
@@ -183,7 +148,6 @@ namespace WindowsFormsApp1
                 txtYard.Text = "";
             }
         }
-
 
         private void btnAllClear_Click(object sender, EventArgs e)
         {
@@ -194,5 +158,23 @@ namespace WindowsFormsApp1
             txtFt.Text = "";
             txtYard.Text = "";
         }
+
+        // 設計一個單位轉換計算的函式，沒有回傳值，設計兩個參數，1.類別參數、2.數值參數
+        private void caculateAnswer(int _kind, double _value)
+        {
+            if (_kind != 0)
+                txtCM.Text = string.Format("{0:0.##########}", _value);
+            if (_kind != 1)
+                txtM.Text = string.Format("{0:0.##########}", _value / 100);
+            if (_kind != 2)
+                txtKM.Text = string.Format("{0:0.##########}", _value / 100000);
+            if (_kind != 3)
+                txtIn.Text = string.Format("{0:0.##########}", _value / 2.54);
+            if (_kind != 4)
+                txtFt.Text = string.Format("{0:0.##########}", _value / 30.48);
+            if (_kind != 5)
+                txtYard.Text = string.Format("{0:0.##########}", _value / 91.44);
+        }
+
     }
 }
